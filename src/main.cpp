df4f324f33cd9583cb7ea8505b5f9577aec704b8
Roman <ilj900@gmail.com>
2021-03-22 16:21:07 +0300

    Base.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
+
 #include <iostream>
-#include "main.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* Window = glfwCreateWindow(1920, 1080, "Vulkan window", nullptr, nullptr);
+
+    uint32_t ExtensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &ExtensionCount, nullptr);
+
+    std::cout << ExtensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(Window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(Window);
+
+    glfwTerminate();
+
+
     return 0;
 }
